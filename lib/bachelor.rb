a3d282d5198ad70bd5f34de3a4d4ebf5a4c81d12
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-07 01:50:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_occupation(data, hometown)
   data.each do |season, array|
     array.each do |people|
       people.each do |key, value|
-        person = value.find()
+        person = value.find {|i| i == hometown}
 end
 
 def get_average_age_for_season(data, season)
