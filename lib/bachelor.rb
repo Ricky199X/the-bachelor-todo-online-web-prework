7dcbaf7a726a998c1204a3eed47e40cc7956b894
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-07 01:49:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -46,7 +46,10 @@ end
 # Build a method get_occupation, that takes in two arguments––the data hash and a string of a hometown. It returns the occupation of the first contestant who hails from that hometown.
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |key, value|
+        person = value.find()
 end
 
 def get_average_age_for_season(data, season)
