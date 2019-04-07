9f59e81b88fb4b3b85e3854577a00fe1221ee8cd
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-07 01:04:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,9 +10,23 @@ def get_first_name_of_season_winner(data, season)
   end   
 end
 
+
+# Build a method, get_contestant_name, that takes in the data hash and an occupation string and returns the name of the woman who has that occupation.
+
 def get_contestant_name(data, occupation)
-  # code here
+  data[occupation].each do |person|
+    person.each do |k, v|
+      if v == "Occupation"
+        contestant = person["name"]
+      end
+    end
+  end
+  contestant
 end
+
+
+
+
 
 def count_contestants_by_hometown(data, hometown)
   # code here
