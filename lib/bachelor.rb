8624930d56981073d7412896941b821c2b19b1ea
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-07 01:28:08 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -30,7 +30,17 @@ end
 # Build a method, count_contestants_by_hometown, that takes in two arguments––the data hash and a string of a hometow. This method should return a counter of the number of contestants who are from that hometown.
 
 def count_contestants_by_hometown(data, hometown)
-  data.each do ||
+  hometown_count = 0
+  data.each do |season, array|
+    array.each do |person|
+      person.each do |key, value|
+        if value == hometown
+          hometown_count += 1
+        end
+      end  
+    end
+  end
+  hometown_count
 end
 
 def get_occupation(data, hometown)
